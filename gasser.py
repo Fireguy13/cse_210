081df35e2a8e7d0f14588ea55a31bd6505c2829f
Fireguy13 <104537058+Fireguy13@users.noreply.github.com>
2022-05-20 14:02:39 -0600

    first commit

---
+++ gasser.py
@@ -0,0 +1,22 @@
+import random
+
+class Guesser: 
+    words = ("tree","mars","class", "scissor", "papper", "rock" "fire" )
+    def __init__(self):
+        self.random = ''
+
+    def get_ramdom_word(self):
+        self.random = self.get_word()
+
+    def get_word(self):
+        word = random.choice(self.words)
+        return word
+
+    def guess(self,letter):
+        indices_of_letter = []
+        for i, ch in enumerate(self.random):
+            if ch == letter:
+                indices_of_letter.append(i)
+        temp = indices_of_letter
+        return temp
+
